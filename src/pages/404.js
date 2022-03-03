1dff6bae83cb94821c5b1137b831d2f20848d1cd
Ridwan Satria <73154922+riidwansatria@users.noreply.github.com>
2022-03-03 17:20:50 +0900

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,14 +4,26 @@ import { graphql } from "gatsby"
 import Layout from "../components/templates/layout"
 import Seo from "../components/seo"
 
+import animation from '/src/images/404-animation.gif'
+
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <main className="grid grid-cols-2 max-w-6xl mx-auto items-center">
+        <img
+        className="w-full px-8"
+        src={animation}
+        alt="404 not found"
+        />
+        <div className="px-8">
+          <h1 className="text-2xl text-neutral-700 font-bold py-4">Ooooops! The page you were looking for couldn't be found</h1>
+          <p className="text-neutral-400">You can navigate through our menu or use this search bar:</p>
+        </div>
+      </main>
+      
     </Layout>
   )
 }
